9d512f3306ad61fa11e7f80c6dc79d2bafcb307f
thytran142 <thytran142@hotmail.com>
2023-01-01 23:11:09 -0500

    Commit to checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {FlexModule} from "@angular/flex-layout";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { PageComponent } from './pages/page/page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {HomeComponent} from "./pages/home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    FlexModule,
+    FlexModule,
+    FlexModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
